63eca0ff2e15a3773c4dc04278590ab8385a3761
jordan carlson & cory ribson <student@epicodus.com>
2015-04-23 15:45:33 -0700

    fix names of folder

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,13 @@
 class String
-  define_method(:anagram) do
+  define_method(:anagram) do |check|
+    key = self.split("").sort()
+    output = ""
+    array_words = check.split()
+    array_words.each() do |element|
+      sorted_element = element.split("").sort()
+      if key == sorted_element
+        output = output.concat(element)
+      end
+    end
   end
 end
-  
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,6 +3,6 @@ require('anagram')
 
 describe('String#anagram') do
   it("will compare two inputs to determine if the 2nd contains an anagram of the first") do
-    expect(("poop", "oopp turd ppoo duty").anagram).to(eq(["oopp", "ppoo"]))
+    expect(("poop").anagram("oopp turd ppoo duty")).to(eq(["oopp", "ppoo"]))
   end
 end
